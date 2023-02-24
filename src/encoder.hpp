eb4ac3045a1290e4e9ad2d8dec15f5053a1adc08
Geeoon <geeoon13@gmail.com>
2023-02-24 08:30:51 -0800

    Used PIMPL to avoid import

--- src/encoder.hpp
+++ src/encoder.hpp
@@ -2,40 +2,44 @@
 #ifndef ENCODER_HPP_
 #define ENCODER_HPP_
 
+#include <memory>
 #include <stdint.h>
 
 extern "C" {
 #include <x264.h>
-#include <libswscale/swscale.h>
+
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavformat/avio.h>
 #include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
 }
 
 namespace h264encoder {
 
+class EncoderImpl;
+
+// simplified version of the x264_nal_t struct containing only the necessary fields for
+// encoding
+typedef struct x264_nal_t_simple {
+	/* Size of payload (including any padding) in bytes. */
+	int i_payload;
+	/* If param->b_annexb is set, Annex-B bytestream with startcode.
+	 * Otherwise, startcode is replaced with a 4-byte size.
+	 * This size is the size used in mp4/similar muxing; it is equal to i_payload-4 */
+	uint8_t* p_payload;
+} x264_nal_t_simple;
+
 class Encoder {
-private:
-	int in_xres, in_yres, out_xres, out_yres;
-	int framecounter;
-	int nheader;
-	x264_t* enc;
-	x264_param_t prms;
-	x264_picture_t pic_in, pic_out;
-
-	struct SwsContext* sws;
-	AVFrame pic_raw;     /* used for our "raw" input container */
-	AVPixelFormat cam_pixel_fmt = AV_PIX_FMT_BGR24;
-
-
 public:
-	x264_nal_t* nals;
+	x264_nal_t_simple* nals;
 	int num_nals;
-	virtual ~Encoder();
 	Encoder(){};
 	Encoder(int, int, int, int, float);
-	int encode(unsigned char *, bool*);
+	int encode(unsigned char*);
+
+private:
+	std::shared_ptr<EncoderImpl> impl;
 };
 
 } // namespace h264encoder
